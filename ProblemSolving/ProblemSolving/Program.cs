e190257abf25bb176b72cdc3194e647711bf6756
Aziza Abdurakhmonova <aziza.abdurakhmanova.iut@gmail.com>
2024-05-15 11:24:07 +0500

    Innital Problem Solving commit

---
+++ ProblemSolving/ProblemSolving/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProblemSolving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Problem Solving!");
+        }
+    }
+}
